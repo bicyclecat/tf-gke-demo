efaeb31dc37d72f28426c2f9df33f648f7130b74
bicyclecat <edelweissman@yahoo.com>
2023-12-14 16:17:21 +0200

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "GCP project name"
+}
+
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = "us-central1"
+  description = "GCP region to use"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  default     = 3
+  description = "GKE nodes number"
+}
