0b54119b70cde7d92edab18ad1bb935fc01e2f37
bicyclecat <edelweissman@yahoo.com>
2023-12-14 16:35:31 +0200

    add machine type var, defaulted to e2-micro

--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ module "gke_cluster" {
   source         = "github.com/bicyclecat/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_MACHINE_TYPE = "e2-micro"
   GKE_NUM_NODES  = 2
 }
--- variables.tf
+++ variables.tf
@@ -9,6 +9,12 @@ variable "GOOGLE_REGION" {
   description = "GCP region to use"
 }
 
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+  default     = "e2-micro"
+  description = "Machine type"
+}
+
 variable "GKE_NUM_NODES" {
   type        = number
   default     = 3
