a164c8c1e708971e41be1bf31435dcb12ae94296
bicyclecat <edelweissman@yahoo.com>
2023-12-14 16:38:01 +0200

    ran terraform fmt, main changed

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "gke_cluster" {
-  source         = "github.com/bicyclecat/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  source           = "github.com/bicyclecat/tf-google-gke-cluster"
+  GOOGLE_REGION    = var.GOOGLE_REGION
+  GOOGLE_PROJECT   = var.GOOGLE_PROJECT
   GKE_MACHINE_TYPE = "e2-micro"
-  GKE_NUM_NODES  = 2
+  GKE_NUM_NODES    = 2
 }
